6fb53fd900903737da7d0de916f5f67df1c87425
sparrshaaa <sparshauk0207@gmail.com>
2024-10-07 20:28:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('.book-now-button').click(function() {
+        // Handle booking logic here, e.g., show a modal or navigate to a booking page
+        alert('Booking functionality goes here!');
+    });
+});
